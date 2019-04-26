9eae8f2d1fb140ed71ba6ca071f640af915b7921
Donald Whitely <45896739+dswhitely1@users.noreply.github.com>
2019-04-26 15:04:07 -0400

    Services Done

--- homesite/src/components/App.jsx
+++ homesite/src/components/App.jsx
@@ -2,6 +2,8 @@ import React, { Component } from 'react';
 import Navigation from './index/Navigation';
 import Header from './index/Header';
 import About from './index/About';
+import Services from './index/Services';
+
 import '../emergency_electric.min.css';
 export class App extends Component {
 	render() {
@@ -10,6 +12,7 @@ export class App extends Component {
 				<Navigation />
 				<Header />
 				<About />
+				<Services />
 			</div>
 		);
 	}

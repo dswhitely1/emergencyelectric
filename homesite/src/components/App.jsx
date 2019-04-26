a77744c0e2b317af552f22607c3b554e40e3ab4f
Donald Whitely <45896739+dswhitely1@users.noreply.github.com>
2019-04-26 13:47:47 -0400

    Navigation Complete

--- homesite/src/components/App.jsx
+++ homesite/src/components/App.jsx
@@ -1,12 +1,8 @@
 import React, { Component } from 'react';
-
+import Navigation from './index/Navigation';
 export class App extends Component {
 	render() {
-		return (
-			<div>
-				<h1>App</h1>
-			</div>
-		);
+		return <Navigation />;
 	}
 }
 
---
+++ homesite/src/components/index/Navigation.jsx
@@ -0,0 +1,52 @@
+import React from 'react';
+import Navbar from 'react-bootstrap/Navbar';
+import Container from 'react-bootstrap/Container';
+
+import Scrollchor from 'react-scrollchor';
+function Navigation() {
+	return (
+		<Navbar
+			bg='success'
+			variant='dark'
+			expand='lg'
+			fixed='top'
+			className='text-uppercase'>
+			<Container className='justify-content-center'>
+				<Navbar.Toggle
+					aria-controls='navbar'
+					className='navbar-toggler-right border=0'
+				/>
+				<Navbar.Collapse
+					id='navbar'
+					className='text-center justify-content-center flex-column-md'>
+					<Scrollchor className='d-lg-none shadow  nav-link text-light mr-0 font-weight-bold navbar-brand'>
+						Emergency Electric Inc
+					</Scrollchor>
+					<Scrollchor to='#page-top' className=' nav-link text-light'>
+						Home
+					</Scrollchor>
+					<Scrollchor to='#about' className=' nav-link text-light'>
+						About
+					</Scrollchor>
+					<Scrollchor to='#services' className=' nav-link text-light'>
+						Services
+					</Scrollchor>
+					<Scrollchor className='d-none d-lg-inline-block shadow  nav-link text-light mr-0 font-weight-bold navbar-brand'>
+						Emergency Electric Inc
+					</Scrollchor>
+					<Scrollchor to='#testimonials' className=' nav-link text-light'>
+						Testimonials
+					</Scrollchor>
+					<Scrollchor to='#goals' className=' nav-link text-light'>
+						Goals
+					</Scrollchor>
+					<Scrollchor to='#contact' className=' nav-link text-light'>
+						Contact
+					</Scrollchor>
+				</Navbar.Collapse>
+			</Container>
+		</Navbar>
+	);
+}
+
+export default Navigation;

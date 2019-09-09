1a659f01be61d88b688e51a7b641ec48022c05f7
Donald Whitely <dswhitely1@gmail.com>
2019-09-09 14:18:55 -0400

    Refactor Contact to Hooks

--- homesite/src/components/index/Contact.jsx
+++ homesite/src/components/index/Contact.jsx
@@ -1,5 +1,5 @@
-import React, { Component } from 'react';
-import { connect } from 'react-redux';
+import React, {useContext} from 'react';
+import useForm from '../../hooks/useForm';
 import Container from 'react-bootstrap/Container';
 import Form from 'react-bootstrap/Form';
 import Button from 'react-bootstrap/Button';
@@ -8,156 +8,119 @@ import Col from 'react-bootstrap/Col';
 import Scrollchor from 'react-scrollchor';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faAngleDoubleUp } from '@fortawesome/free-solid-svg-icons';
-import {
-	formValidation,
-	resetContactForm,
-	valueContactFormChange,
-	sendMessage,
-	showMessageDisplay,
-	updateMessageDisplay,
-	updateFormButton,
-} from '../../actions';
+import { ActionsContext } from '../../contexts/ActionsContext';
 
-class Contact extends Component {
-	componentWillMount() {
-		this.props.updateFormButton(false);
-	}
-	handleSubmit(e) {
-		this.props.updateFormButton(true);
-		const form = e.currentTarget;
-		e.preventDefault();
-		e.stopPropagation();
-		if (!form.checkValidity()) {
-			e.preventDefault();
-			e.stopPropagation();
-		}
-		this.props.formValidation(true);
-		const values = {
-			firstname : this.props.messageForm.firstName,
-			lastname  : this.props.messageForm.lastName,
-			email     : this.props.messageForm.email,
-			subject   : this.props.messageForm.subject,
-			message   : this.props.messageForm.message,
-		};
+function Contact () {
+  const [values, handleChange, handleSubmit, handleReset] = useForm({
+    first_name: '',
+    last_name: '',
+    email: '',
+    subject: '',
+    message: '',
+  }, submitValues);
 
-		if (form.checkValidity()) {
-			this.props.sendMessage(values);
-			this.props.resetContactForm();
-		}
-	}
-	handleClearForm = () => {
-		this.props.resetContactForm();
-	};
-	handleFirstNameChange = e => {
-		this.props.valueContactFormChange('firstName', e.target.value);
-		this.props.showMessageDisplay(false);
-	};
+  function submitValues () {
+    sendMessage(values);
+  }
 
-	handleLastNameChange = e => {
-		this.props.valueContactFormChange('lastName', e.target.value);
-		this.props.showMessageDisplay(false);
-	};
+  const { first_name, last_name, email, subject, message } = values;
+  const {message: {sendMessage}} = useContext(ActionsContext);
+  return (
+    <section
+      className='ee-content-section bg-light text-dark'
+      id='contact'
+    >
+      <Container>
+        <h3 className='mb-1 text-center'>Contact Us</h3>
+        <h2 className='mb-5 text-center'>Send Us a Message!</h2>
+        <Form onSubmit={handleSubmit}>
+          <Form.Group controlId='formContact'>
+            <Row className='mb-2'>
+              <Col
+                md={6}
+                className='mb-sm-2 mb-md-0'
+              >
+                <Form.Control
+                  required
+                  value={first_name}
+                  type='text'
+                  placeholder='First Name'
+                  name='first_name'
+                  onChange={handleChange}
+                />
+                <Form.Control.Feedback type='invalid'>Required</Form.Control.Feedback>
+              </Col>
+              <Col md={6}>
+                <Form.Control
+                  required
+                  name='last_name'
+                  value={last_name}
+                  type='text'
+                  placeholder='Last Name'
+                  onChange={handleChange}
+                />
+                <Form.Control.Feedback type='invalid'>Required</Form.Control.Feedback>
+              </Col>
+            </Row>
+            <Form.Control
+              required
+              className='mb-2'
+              type='email'
+              value={email}
+              name='email'
+              placeholder='Email Address'
+              onChange={handleChange}
+            />
+            <Form.Control.Feedback type='invalid'>Please provide a valid email
+              address</Form.Control.Feedback>
+            <Form.Control
+              required
+              name='subject'
+              value={subject}
+              type='text'
+              className='mb-2'
+              placeholder='Subject'
+              onChange={handleChange}
+            />
+            <Form.Control.Feedback type='invalid'>Required</Form.Control.Feedback>
+            <Form.Control
+              as='textarea'
+              className='mb-2'
+              rows={4}
+              placeholder='Enter Message Here'
+              required
+              name='message'
+              value={message}
+              onChange={handleChange}
+            />
+            <Button
+              type='submit'
+              variant='warning'
+              className='mr-2'
+              size='lg'
+            >
+              Send Message
+            </Button>
+            <Button
+              type='button'
+              variant='outline-warning'
+              size='lg'
+              onClick={handleReset}
+            >
+              Clear Form
+            </Button>
 
-	handleEmailChange = e => {
-		this.props.valueContactFormChange('email', e.target.value);
-		this.props.showMessageDisplay(false);
-	};
-	handleSubjectChange = e => {
-		this.props.valueContactFormChange('subject', e.target.value);
-		this.props.showMessageDisplay(false);
-	};
-	handleMessageChange = e => {
-		this.props.valueContactFormChange('message', e.target.value);
-		this.props.showMessageDisplay(false);
-	};
-	render() {
-		const { validated, firstName, lastName, email, subject, message } = this.props.messageForm;
-		return (
-			<section className='ee-content-section bg-light text-dark' id='contact'>
-				<Container className=''>
-					<h3 className='mb-1 text-center'>Contact Us</h3>
-					<h2 className='mb-5 text-center'>Send Us a Message!</h2>
-					<Form noValidate validated={validated} onSubmit={e => this.handleSubmit(e)}>
-						<Form.Group controlId='formContact'>
-							<Row className='mb-2'>
-								<Col md={6} className='mb-sm-2 mb-md-0'>
-									<Form.Control
-										required
-										value={firstName}
-										type='text'
-										placeholder='First Name'
-										onChange={this.handleFirstNameChange}
-									/>
-									<Form.Control.Feedback type='invalid'>Required</Form.Control.Feedback>
-								</Col>
-								<Col md={6}>
-									<Form.Control
-										required
-										value={lastName}
-										type='text'
-										placeholder='Last Name'
-										onChange={this.handleLastNameChange}
-									/>
-									<Form.Control.Feedback type='invalid'>Required</Form.Control.Feedback>
-								</Col>
-							</Row>
-							<Form.Control
-								required
-								className='mb-2'
-								type='email'
-								value={email}
-								placeholder='Email Address'
-								onChange={this.handleEmailChange}
-							/>
-							<Form.Control.Feedback type='invalid'>Please provide a valid email address</Form.Control.Feedback>
-							<Form.Control
-								required
-								value={subject}
-								type='text'
-								className='mb-2'
-								placeholder='Subject'
-								onChange={this.handleSubjectChange}
-							/>
-							<Form.Control.Feedback type='invalid'>Required</Form.Control.Feedback>
-							<Form.Control
-								as='textarea'
-								className='mb-2'
-								rows={4}
-								placeholder='Enter Message Here'
-								required
-								value={message}
-								onChange={this.handleMessageChange}
-							/>
-						</Form.Group>
-						<Button type='submit' variant='warning' className='mr-2' size='lg'>
-							{
-								this.props.messageAlert.isSending ? `Sending` :
-								`Send Message`}
-						</Button>
-						<Button type='button' variant='outline-warning' size='lg' onClick={this.handleClearForm}>
-							Clear Form
-						</Button>
+            <Scrollchor
+              to='#page-top'
+              className='btn btn-warning btn-lg float-right'
+            >
+              <FontAwesomeIcon icon={faAngleDoubleUp} />
+            </Scrollchor>
 
-						<Scrollchor to='#page-top' className='btn btn-warning btn-lg float-right'>
-							<FontAwesomeIcon icon={faAngleDoubleUp} />
-						</Scrollchor>
-					</Form>
-				</Container>
-			</section>
-		);
-	}
+          </Form.Group>
+        </Form>
+      </Container>
+    </section>
+  );
 }
-
-const mapStateToProps = state => {
-	return { messageForm: state.formContact, messageAlert: state.messageDisplay };
-};
-
-export default connect(mapStateToProps, {
-	formValidation,
-	resetContactForm,
-	valueContactFormChange,
-	sendMessage,
-	showMessageDisplay,
-	updateMessageDisplay,
-	updateFormButton,
-})(Contact);
+export default Contact;
